c345d43bfbf99d24c63cf5ebc40bfea3b324fd12
AngeloGalav <tekken342@gmail.com>
2024-11-19 19:36:58 +0100

    batch_generate_chat fixes
    - remove unused imports
    - remove erroneous token in get_formatted_chat_input
    - fix tqdm import
    - decreased n_samples to 50
    - fix missing file pathname
    - remove legacy cache warning
    - changed generate_long_answer_predictions_llama to generate a single answer
    - fix execution time saving
    
    Running this sh*t again for the last time since it is taking me so long.
    NEVER RUN UNTESTED LONG SCRIPTS ON THE WEEKEND.

--- batch_generate_chat.py
+++ batch_generate_chat.py
@@ -5,11 +5,10 @@ import os
 import csv
 import torch
 from configurations import configurations
-from chat_eval_utils import get_formatted_chat_input
 from transformers import AutoTokenizer
 import modified_llama
 import time
-import tqdm
+from tqdm import tqdm
 import chat_eval_utils
 
 model_name = "meta-llama/Llama-2-7b-chat-hf"
@@ -30,9 +29,6 @@ best_configurations = ['baseline',
                   '2_3rds_plus_llama',
                   'skip_near_end_keep_last_two']
 
-print("doing ony best configs ", best_configurations)
-
-
 # all outputs are going into the output folder
 output_folder = "outputs2"
 if not os.path.exists(output_folder):
@@ -45,7 +41,7 @@ for conf in best_configurations:
         csv_writer = csv.writer(csvfile)
         csv_writer.writerow(["configuration", "execution_time", "question", "generated_text"])
 
-max_len_data=100
+max_len_data=50
 questions = chat_eval_utils.load_nq_dataset_from_parquet("nq_dataset_partial/validation-00000-of-00007.parquet")[:max_len_data]
 
 for config in configurations:
@@ -57,27 +53,22 @@ for config in configurations:
             num_repeats=config["repeats"],
             skip_all=False
         )
+        for question in tqdm(questions):
+            start_time = time.time()
+            answer = chat_eval_utils.generate_long_answer_predictions_llama(franken_llama, tokenizer, question, device)
+            end_time = time.time()
+            execution_time = end_time - start_time
 
-        start_time = time.time()
-        answers = chat_eval_utils.generate_long_answer_predictions_llama(franken_llama, tokenizer, questions, device)
-        end_time = time.time()
-        execution_time = end_time - start_time
-
-        cleaned_answers = []
-        # generate and cleanup text
-        for i in answers  :
-            cleaned_text = i.replace('\n', ' ').strip()
+            # generate and cleanup text
+            cleaned_text = answer.replace('\n', ' ').strip()
             cleaned_text = cleaned_text.encode("ascii", "replace").decode("ascii")
-            wrapped_text = f'"{cleaned_text}"'
-            cleaned_answers.append(wrapped_text)
+            cleaned_text = f'"{cleaned_text}"'
 
-        # save generated text to CSV
-        with open(csv_filename, "a", newline='') as csvfile:
-            csv_writer = csv.writer(csvfile)
-            for question, answer in zip(questions, cleaned_answers):
-                csv_writer.writerow([config["name"], execution_time, question, answer])
-
-            print(f"Generated text from {config["name"]} saved to csv")
+            # save generated text to CSV
+            csv_filename = output_folder+"/"+f"{config["name"]}.csv"
+            with open(csv_filename, "a", newline='') as csvfile:
+                csv_writer = csv.writer(csvfile)
+                csv_writer.writerow([config["name"], execution_time, question, cleaned_text])
 
         print(f"Configuration {config['name']} processed and saved.")
 
--- chat_eval_utils.py
+++ chat_eval_utils.py
@@ -1,8 +1,7 @@
 import pandas as pd
-import tqdm
 
 def get_formatted_chat_input(input_text):
-    final_text=f"""<s>[INST] <<SYS>>
+    final_text=f"""[INST] <<SYS>>
     You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe.  Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
 
     If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
@@ -16,14 +15,11 @@ def load_nq_dataset_from_parquet(parquet_file):
     questions = df['question'].apply(lambda x: x['text'] if isinstance(x, dict) and 'text' in x else "")
     return questions
 
-def generate_long_answer_predictions_llama(model, tokenizer, questions, device, max_length=100):
-    predictions = []
-    for question in tqdm(questions):
-        input_text = get_formatted_chat_input(question)
-        input_ids = tokenizer(input_text, return_tensors='pt').input_ids.to(device)
-        outputs = model.generate(input_ids, max_new_tokens=max_length, return_dict_in_generate=True)
-        generated_ids = outputs.sequences
-        new_tokens = generated_ids[0][input_ids.shape[-1]:]
-        answers = tokenizer.decode(new_tokens, skip_special_tokens=True)
-        predictions.append(answers)
-    return predictions
+def generate_long_answer_predictions_llama(model, tokenizer, question, device, max_length=100):
+    input_text = get_formatted_chat_input(question)
+    input_ids = tokenizer(input_text, return_tensors='pt').input_ids.to(device)
+    outputs = model.generate(input_ids, max_new_tokens=max_length, return_legacy_cache=True, return_dict_in_generate=True)
+    generated_ids = outputs.sequences
+    new_tokens = generated_ids[0][input_ids.shape[-1]:]
+    answer = tokenizer.decode(new_tokens, skip_special_tokens=True)
+    return answer
